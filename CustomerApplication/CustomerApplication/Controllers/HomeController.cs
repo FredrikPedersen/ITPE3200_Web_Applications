bad54efcf0844e81ad384be7d91cf0c591aeea1e
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-19 17:06:14 +0200

    Did something

---
+++ CustomerApplication/CustomerApplication/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomerApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session["Customers"] = new List<Models.Customer>();
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.Customer customerIn)
+        {
+            var allCustomers = (List<Models.Customer>)Session["Customers"];
+            allCustomers.Add(customerIn);
+            Session["Customers"] = allCustomers;
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            var allCustomers = (List<Models.Customer>)Session["Customers"];
+            return View(allCustomers);
+        }
+
+        public ActionResult Delete(String deleteName)
+        {
+            var allCustomers = (List<Models.Customer>)Session["Customers"];
+
+            for (var i = 0; i < allCustomers.Count; i++)
+            {
+                if (allCustomers[i].Name == deleteName)
+                {
+                    allCustomers.RemoveAt(i);
+                }
+            }
+
+            Session["Customers"] = allCustomers;
+
+            return RedirectToAction("List");
+        }
+    }
+}
