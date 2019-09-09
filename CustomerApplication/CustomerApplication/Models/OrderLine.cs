1293c32be0e4bcd430e8578a75084bc975d3be89
FredrikPedersen <fredrikhp@gmail.com>
2019-09-09 17:55:11 +0200

    Added lecture examples of retrieval and manipulation of data from database

--- CustomerApplication/CustomerApplication/Models/OrderLine.cs
+++ CustomerApplication/CustomerApplication/Models/OrderLine.cs
@@ -7,7 +7,7 @@ namespace CustomerApplication.Models
 {
     public class OrderLine
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public int Quantity { get; set; }
         public virtual Product Product { get; set; }
         public virtual Order Order { get; set; }
