999991d927724ab2535878eebbfb57de9f67f8b5
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-23 15:30:26 +0200

    Added lecture project MVC-Ajax-1

---
+++ LectureProjects/MVC-Ajax-1/MVC-Ajax/MVC-Ajax/Controllers/KundeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Ajax.Models;
+using System.Web.Script.Serialization;
+
+namespace MVC_Ajax.Controllers
+{
+    public class KundeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string hentAlleNavn()
+        {
+            var db = new KundeDB();
+            List <kunde> alleKunder= db.hentAlleKunder();
+            
+            var alleNavn = new List<jsKunde>();
+            foreach (kunde k in alleKunder)
+            {
+                var ettNavn = new jsKunde();
+                ettNavn.id = k.id;
+                ettNavn.navn = k.fornavn + " " + k.etternavn;
+                alleNavn.Add(ettNavn);
+            }
+            var jsonSerializer = new JavaScriptSerializer();
+            string json = jsonSerializer.Serialize(alleKunder);
+            return json;
+        }
+
+        public string hentKundeInfo(int id)
+        {
+            var db = new KundeDB();
+            kunde enKunde = db.hentEnKunde(id);
+            var jsonSerializer = new JavaScriptSerializer();
+            string json = jsonSerializer.Serialize(enKunde);
+            return json;
+        }
+
+        public string register(kunde innKunde)
+        {
+            var db = new KundeDB();
+            db.lagreEnKunde(innKunde);
+            var jsonSerializer = new JavaScriptSerializer();
+            return jsonSerializer.Serialize("OK");
+        }
+    }
+}
