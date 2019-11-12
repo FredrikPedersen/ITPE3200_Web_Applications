d64f63b439c2ead426128a599b1b296268494115
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 18:12:49 +0100

    Added QuestionButton component

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/UI/QuestionButton/QuestionButton.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import "./QuestionButton.css";
+
+const questionButton = (props) => (
+    <dt>
+        <button 
+            className="QuestionButton"
+            aria-expanded={props.isOpen}
+            aria-controls={props.answerId}
+            onClick={props.onToggle} 
+        >{props.children} </button>
+    </dt>
+);
+
+export default questionButton;
