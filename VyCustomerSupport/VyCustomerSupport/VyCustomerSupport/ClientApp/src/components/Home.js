3a43fbfc70e20d42274bc1d983458b6cb945c14a
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 12:46:43 +0100

    Testing if communication between Frontend and Backend works

--- VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/Home.js
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/Home.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            qas: []
+        };
+
+        fetch("api/index/qas")
+            .then(response => response.json())
+            .then(qas => {
+                this.setState({qas: qas});
+                console.log(this.state.qas);
+            });
+        
+    }
+
+  render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
 }
