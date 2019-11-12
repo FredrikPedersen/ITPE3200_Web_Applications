c3c855f1d857c70a40ea758fd5b8bc046085e5c2
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 12:18:18 +0100

    Added BLL for QAs

---
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/BLL/QaBll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using VyCustomerSupport.DAL.Repositories;
+using VyCustomerSupport.Models.RepositoryModels;
+
+namespace VyCustomerSupport.BLL
+{
+    public class QaBll
+    {
+        private readonly QaRepository _qaRepository;
+
+        public QaBll(QaRepository qaRepository)
+        {
+            _qaRepository = qaRepository;
+        }
+
+        public List<RepositoryQa> GetAllQa()
+        {
+            return _qaRepository.GetAllQa();
+        }
+    }
+}
