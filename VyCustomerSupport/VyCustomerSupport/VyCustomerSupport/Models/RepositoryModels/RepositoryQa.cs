5d75149a59b86fe48de664a8e703ff83d7c1d680
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 12:17:37 +0100

    Added Repository Models for QAs and Questions

---
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/Models/RepositoryModels/RepositoryQa.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VyCustomerSupport.Models.RepositoryModels
+{
+    public class RepositoryQa
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int UpVotes { get; set; }
+        public int DownVotes { get; set; }
+    }
+}
---
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/Models/RepositoryModels/RepositoryQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VyCustomerSupport.Models.RepositoryModels
+{
+    public class RepositoryQuestion
+    {
+        [Required]
+        public string Question { get; set; }
+        
+        [Required]
+        public string Username { get; set; }
+    }
+}
