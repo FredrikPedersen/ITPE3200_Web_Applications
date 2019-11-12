dfd0aac85517ee75585c95d6bed3c1b24c990ec9
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 10:46:46 +0100

    Added DatabaseContext class

---
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/DAL/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VyCustomerSupport.Models.DbModels;
+
+namespace VyCustomerSupport.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<DbQA> QandA { get; set; }
+    }
+}
