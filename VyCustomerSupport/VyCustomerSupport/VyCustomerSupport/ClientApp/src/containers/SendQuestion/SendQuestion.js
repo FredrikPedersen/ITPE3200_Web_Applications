6b2e77e272164aa6ceea35ba74182e3884493d37
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 14:15:01 +0100

    Added containers for sending in a new question and testing

---
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SendQuestion/SendQuestion.js
@@ -0,0 +1,11 @@
+﻿import React, {Component} from "react";
+
+export class SendQuestion extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
