0f895d4e015bdd3ec0116ca209dc336a88b3df27
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 18:13:10 +0100

    Added Headline component

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/Headline/Headline.js
@@ -0,0 +1,11 @@
+﻿﻿import React from "react";
+import "./Headline.css";
+
+const headline = (props) => (
+    <div className="Headline">
+        <h1>{props.children}</h1>
+        <p>{props.infoText}</p>
+    </div>
+);
+
+export default headline;
