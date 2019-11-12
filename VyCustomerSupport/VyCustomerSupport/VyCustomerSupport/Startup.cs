cf67f9d7d043ca33648e1f3e03f01867d10ee353
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-12 10:51:06 +0100

    Added DatabaseContext and DatabaseInitialization to the Startup

--- VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/Startup.cs
+++ VyCustomerSupport/VyCustomerSupport/VyCustomerSupport/Startup.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VyCustomerSupport.DAL;
 
 namespace VyCustomerSupport
 {
@@ -21,7 +23,10 @@ namespace VyCustomerSupport
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            const string connection = @"Server=(localdb)\mssqllocaldb;Database=SupportDatabase;Trusted_Connection=True;ConnectRetryCount=0";
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connection));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -61,6 +66,16 @@ namespace VyCustomerSupport
                     spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
+
+            InitializeMigrations(app);
+        }
+
+        private static void InitializeMigrations(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                DbInit.Initialize(serviceScope);
+            }
         }
     }
 }
