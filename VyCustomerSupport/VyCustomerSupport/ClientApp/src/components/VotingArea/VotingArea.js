37fa15afa4ca01497645eb7b461e1ed6a60a2dea
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-13 15:39:45 +0100

    Added VotingArea Component

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/VotingArea/VotingArea.js
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import "./VotingArea.css";
+
+const votingArea = (props) => {
+    let output = null;
+
+    props.voted ?
+        (output = (
+                <div className="VotingArea">
+                    <img className="VoteArrow" src="https://i.imgur.com/sPbn20T.png"/>
+                    <p className="Positive"><strong>{props.upVotes}</strong></p>
+                    <img className="VoteArrow" src="https://i.imgur.com/505R8AT.png"/>
+                    <p className="Negative"><strong>{props.downVotes}</strong></p>
+                </div>)
+        ) :
+        (output =
+            (<div className="VotingArea">
+                    <i>Synes du dette var nyttig?</i>
+                    <img onClick={props.upVoteClick} className="VoteArrow" src="https://i.imgur.com/sPbn20T.png"/>
+                    <img onClick={props.downVoteClick} className="VoteArrow" src="https://i.imgur.com/505R8AT.png"/>
+                </div>
+            ));
+
+    return output;
+};
+
+export default votingArea;
