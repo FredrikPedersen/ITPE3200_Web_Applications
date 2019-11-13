639f79d4d2da77f02553ffc2fcd2f4fdc85fa5c1
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-13 16:10:34 +0100

    Added Header to send in Questions

--- VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SendQuestion/SendQuestion.js
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SendQuestion/SendQuestion.js
@@ -1,10 +1,17 @@
 ﻿﻿import React, {Component} from "react";
+import "../../Styles.css";
+import Header from "../../components/Header/Header";
+
+const SEND_INFO_TEXT = "Noe du lurer på som ikke er besvart fra før? Send det inn her! Vi vil svare på så mange vi kan når vi har kapasitet.";
+const TITLE = "Send inn Spørsmål";
 
 export class SendQuestion extends Component {
     render() {
         return(
-            <div>
-                
+            <div className="ContentArea">
+                <Header
+                infoText={SEND_INFO_TEXT}
+                >{TITLE}</Header>
             </div>
         );
     }
