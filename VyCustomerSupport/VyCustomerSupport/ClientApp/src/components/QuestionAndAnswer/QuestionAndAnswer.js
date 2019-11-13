88ef7c5812d796d21270e826c1a3e3f345863ed2
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-13 10:18:40 +0100

    Added QuestionAndAnswer and QuestionsAndAnswers components

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/QuestionAndAnswer/QuestionAndAnswer.js
@@ -0,0 +1,32 @@
+﻿import React, {Component} from "react";
+
+class QuestionAndAnswer extends Component {
+    state = {
+        expanded: false
+    };
+    
+    expansionHandler = () => {
+      let isExpanded = this.state.expanded;
+      this.setState({expanded: !isExpanded})
+    };
+    
+    render() {
+        let output = (
+            <div className="QuestionButton" onClick={this.expansionHandler}
+            >{this.props.question}
+            </div>
+        );
+        
+        if (this.state.expanded) {
+            output = (
+                <div onClick={this.expansionHandler}>
+                    <div className="Question">{this.props.question}</div>
+                    <div className="Answer">{this.props.answer}</div>
+                </div>
+            );
+        }
+        return output;
+    }
+}
+
+export default QuestionAndAnswer;
---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/QuestionsAndAnswers/QuestionsAndAnswers.js
@@ -0,0 +1,17 @@
+﻿import React, {PureComponent} from "react";
+import QuestionAndAnswer from "../QuestionAndAnswer/QuestionAndAnswer";
+
+class QuestionsAndAnswers extends PureComponent {
+    render() {
+        return this.props.questionsAndAnswers.map(qaa => {
+            return (
+                <QuestionAndAnswer
+                    key={qaa.id}
+                    question={qaa.question}
+                    answer={qaa.answer}/>
+            )
+        })
+    }
+}
+
+export default QuestionsAndAnswers;
