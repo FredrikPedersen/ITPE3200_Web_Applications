ae21781f0059b0c1ebc4c421f74b2dbe5ddaaf6f
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-15 19:47:44 +0100

    Added InputElement Component

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/UI/InputElement/InputElement.js
@@ -0,0 +1,52 @@
+﻿import React from "react";
+import "./InputElement.css";
+
+const inputElement = (props) => {
+    let inputElement = null;
+    let label = <p>{props.label}</p>;
+
+    const inputStyles = ["InputArea"];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputStyles.push("Invalid");
+    }
+
+    if (props.invalid && props.touched) {
+        label = <p className={"ValidationError"}>{props.label} Vennligst oppgi en gyldig verdi!</p>
+    }
+
+    switch (props.elementType) {
+        case ("input"):
+            inputElement = <input
+                className={inputStyles.join(" ")}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}/>;
+            break;
+
+        case ("textarea"):
+            inputElement = <textarea
+                className={inputStyles.join(" ")}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}/>;
+            break;
+
+        default:
+            inputElement = <input
+                className={inputStyles.join(" ")}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+    }
+    
+    return (
+        <div className={"InputElement"}>
+            <label className={"Label"}>{label}</label>
+            {inputElement}
+        </div>
+    );
+};
+
+export default inputElement;
