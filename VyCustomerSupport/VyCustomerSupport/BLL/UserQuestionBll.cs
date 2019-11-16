fa6c8b4c8b9a64d7065e6a14cf21b2db297d7e59
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 11:54:25 +0100

    Added BLL for UserQuestion

---
+++ VyCustomerSupport/VyCustomerSupport/BLL/UserQuestionBll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using VyCustomerSupport.DAL.Repositories;
+using VyCustomerSupport.Models.RepositoryModels;
+
+namespace VyCustomerSupport.BLL
+{
+    public class UserQuestionBll
+    {
+        private readonly UserQuestionRepository _qRepository;
+
+        public UserQuestionBll(UserQuestionRepository qRepository)
+        {
+            _qRepository = qRepository;
+        }
+
+        public List<RepositoryUserQuestion> GetAllUserQuestions()
+        {
+            return _qRepository.GetAllUserQuestions();
+        }
+
+        public bool UpVote(int id)
+        {
+            return _qRepository.UpVote(id);
+        }
+
+        public bool DownVote(int id)
+        {
+            return _qRepository.DownVote(id);
+        }
+    }
+}
