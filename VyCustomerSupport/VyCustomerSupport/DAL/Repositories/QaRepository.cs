a2bddeee424bc38ed513e339a0d23820bb352fa6
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 12:41:58 +0100

    Removed redundant import

--- VyCustomerSupport/VyCustomerSupport/DAL/Repositories/QaRepository.cs
+++ VyCustomerSupport/VyCustomerSupport/DAL/Repositories/QaRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using VyCustomerSupport.Models.DbModels;
 using VyCustomerSupport.Models.RepositoryModels;
