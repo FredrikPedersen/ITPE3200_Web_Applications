e2c3e5a61b300375cd2f5a0a29e2346f4d0c5683
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 17:57:35 +0100

    Added code for displaying all UserQuestions

--- VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SentInQuestions/SentInQuestions.js
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SentInQuestions/SentInQuestions.js
@@ -1,19 +1,50 @@
 ﻿import React, {Component} from "react";
 import "../../Styles.css";
 import Header from "../../components/UI/Header/Header";
+import UserQuestions from "../../components/UserQuesions/UserQuestions";
+import Spinner from "../../components/UI/LoadingSpinner/LoadingSpinner";
+
+const SENT_IN_INFO_TEXT = "Her kan du se brukerinnsendte spørsmål. Vi vil kontinuerlig prøve å besvare relevante spørsmål og legge de til i FAQ-siden.";
+const TITLE = "Innsendte Spørsmål";
 
 export class SentInQuestions extends Component {
     
-    constructor(props)  {
+    constructor(props) {
         super(props);
+        
+        this.state = {
+            questions: [],
+            loading: true,
+            error: false
+        };
+
+        fetch("api/index/userquestions")
+            .then(response => response.json())
+            .then(questions => {
+                this.setState({questions: questions, loading:false});
+            })
+            .catch(error => {
+                this.setState({error: true});
+            })
+    }
+    
+    sentInContent() {
+        return this.state.error ? <p className="ErrorMessage">Kan ikke laste inn spørsmål og svar. Vennligst kontroller Internetttilgangen </p> :
+            this.state.loading ? <Spinner/> : 
+                (<UserQuestions
+                questions={this.state.questions}/>
+        )
     }
     
     render() {
+        const questionsContent = this.sentInContent();
+        
         return(
             <div className="ContentArea">
                 <Header
-                    infoText="Her kan du se brukerinnsendte spørsmål. De med flest positive stemmer vil først bli besvart og lagt til på FAQ-siden"
-                >Innsendte Spørsmål</Header>
+                    infoText={SENT_IN_INFO_TEXT}
+                >{TITLE}</Header>
+                {questionsContent}
             </div>
         )
     }
