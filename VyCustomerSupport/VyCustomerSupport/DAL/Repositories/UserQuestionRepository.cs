88f68f676937eacf5950f5509b79fcf480ec95c4
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 11:54:14 +0100

    Added Repository for UserQuestion

---
+++ VyCustomerSupport/VyCustomerSupport/DAL/Repositories/UserQuestionRepository.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VyCustomerSupport.Models.DbModels;
+using VyCustomerSupport.Models.RepositoryModels;
+
+namespace VyCustomerSupport.DAL.Repositories
+{
+    public class UserQuestionRepository
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public UserQuestionRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public bool AddQa(RepositoryUserQuestion q)
+        {
+            var newUserQuestion = new DbUserQuestion
+            {
+                Username = q.Username,
+                Email = q.Email,
+                Question = q.Question,
+                Upvotes = q.Upvotes,
+                Downvotes = q.Downvotes
+            };
+
+            try
+            {
+                _databaseContext.UserQuestions.Add(newUserQuestion);
+                _databaseContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public List<RepositoryUserQuestion> GetAllUserQuestions()
+        {
+            return _databaseContext.UserQuestions
+                .OrderBy(q => q.Id)
+                .Select(q => DbToRepository(q))
+                .ToList();
+        }
+
+        public bool UpVote(int id)
+        {
+            try
+            {
+                var userQuestion = _databaseContext.UserQuestions.FirstOrDefault(q => q.Id == id);
+                if (userQuestion != null) userQuestion.Upvotes += 1;
+                _databaseContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DownVote(int id)
+        {
+            try
+            {
+                var userQuestion = _databaseContext.UserQuestions.FirstOrDefault(q => q.Id == id);
+                if (userQuestion != null) userQuestion.Downvotes += 1;
+                _databaseContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private RepositoryUserQuestion DbToRepository(DbUserQuestion dbQ)
+        {
+            return new RepositoryUserQuestion
+            {
+                Id = dbQ.Id,
+                Username = dbQ.Username,
+                Email = dbQ.Email,
+                Upvotes = dbQ.Upvotes,
+                Downvotes = dbQ.Downvotes
+            };
+        }
+
+    }
+}
