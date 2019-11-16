a0ad8a0ab81ede11b7db5b7eff35dc8965eab98f
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 17:51:22 +0100

    Added component for UserQuestions

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/UserQuesions/UserQuestions.js
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import UserQuestion from "./UserQuestion/UserQuestion";
+
+const userQuestions = (props) => {
+    return props.questions.map(uq => {
+        return (
+            <UserQuestion
+            key={uq.id}
+            id={uq.id}
+            username={uq.username}
+            email={uq.email}
+            question={uq.question}
+            />
+        )
+    })
+    
+};
+
+export default userQuestions;
