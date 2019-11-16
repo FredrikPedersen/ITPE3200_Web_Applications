945df741ada6d944d41657c31015ebfbe072d73c
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 17:51:10 +0100

    Added component for UserQuestion

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/UserQuesions/UserQuestion/UserQuestion.js
@@ -0,0 +1,11 @@
+﻿import React from "react";
+import "./UserQuestion.css";
+
+const userQuestion = (props) => (
+    <div className="UserQuestionBox">
+        <p className="UserQuestion">"{props.question}"</p>
+        <p className="SentBy">Fra {props.username}, {props.email}</p>
+    </div>
+);
+
+export default userQuestion;
