3b9f4ad9d82fa01083d56b8758c83ee97b746d69
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 11:53:11 +0100

    Renamed RepositoryQuestion to RepositoryUserQuestions. Added more attributes.

---
+++ VyCustomerSupport/VyCustomerSupport/Models/RepositoryModels/RepositoryUserQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VyCustomerSupport.Models.RepositoryModels
+{
+    public class RepositoryUserQuestion
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Question { get; set; }
+        
+        [Required]
+        public int Upvotes { get; set; }
+        
+        [Required]
+        public int Downvotes { get; set; }
+    }
+}
