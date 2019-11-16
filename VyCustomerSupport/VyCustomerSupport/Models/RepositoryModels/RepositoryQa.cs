cd85bc86b229a56509655aa7930867d6513c535b
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 11:56:16 +0100

    Made all fields required in RepositoryQa

--- VyCustomerSupport/VyCustomerSupport/Models/RepositoryModels/RepositoryQa.cs
+++ VyCustomerSupport/VyCustomerSupport/Models/RepositoryModels/RepositoryQa.cs
@@ -4,10 +4,15 @@ namespace VyCustomerSupport.Models.RepositoryModels
 {
     public class RepositoryQa
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Question { get; set; }
+        [Required]
         public string Answer { get; set; }
+        [Required]
         public int UpVotes { get; set; }
+        [Required]
         public int DownVotes { get; set; }
     }
 }
