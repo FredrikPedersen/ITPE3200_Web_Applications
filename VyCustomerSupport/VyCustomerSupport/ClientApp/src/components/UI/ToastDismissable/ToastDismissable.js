624640372f2a93ce4ab6ebbbfdc180cbe310e3de
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 17:53:01 +0100

    Created toast message using MDB React Bootstrap

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/UI/ToastDismissable/ToastDismissable.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from "react";
+import { MDBNotification } from "mdbreact";
+
+//Taken from the template at https://mdbootstrap.com/docs/react/advanced/notifications/
+class ToastDismissable extends Component {
+    render() {
+        return (
+            <MDBNotification
+                show
+                fade
+                iconClassName="text-primary"
+                title={this.props.toastTitle}
+                message={this.props.toastMessage}
+                text="Nå nettopp"
+                style={{position: "absolute", left: "80%", marginLeft: "-40px"}}
+            />
+        );
+    }
+}
+
+export default ToastDismissable;
