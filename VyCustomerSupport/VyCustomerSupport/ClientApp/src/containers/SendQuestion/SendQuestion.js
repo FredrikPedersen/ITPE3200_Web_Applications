3ad3960bf5566d9624e98b0e67ad9050ea3cae93
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 17:58:21 +0100

    Added a Toast message when the form is submitted, and some bugfixes for when the form is cleared.

--- VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SendQuestion/SendQuestion.js
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/SendQuestion/SendQuestion.js
@@ -1,94 +1,95 @@
-﻿﻿import React, {Component} from "react";
+﻿﻿
+import React, {Component} from "react";
 import "../../Styles.css";
 import Header from "../../components/UI/Header/Header";
 import InputElement from "../../components/UI/InputElement/InputElement";
 import Spinner from "../../components/UI/LoadingSpinner/LoadingSpinner";
+import ToastDismissable from "../../components/UI/ToastDismissable/ToastDismissable";
 
 const SEND_INFO_TEXT = "Noe du lurer på som ikke er besvart fra før? Send det inn her! Vi vil svare på så mange vi kan når vi har kapasitet.";
 const TITLE = "Send inn Spørsmål";
+let toast = null;
 
 export class SendQuestion extends Component {
-    state = {
-        formIsValid: false,
-        loading: false,
+    constructor(props) {
+        super(props);
         
-        questionForm: {
-            Brukernavn: {
-                elementType: "input",
-                elementConfig: {
-                    type: "text",
-                    placeholder: "Navn (vil bli vist til andre brukere)"
+        this.initialstate = {
+            formIsValid: false,
+            loading: false,
+
+            questionForm: {
+                Brukernavn: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "text",
+                        placeholder: "Navn"
+                    },
+                    value: "",
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
-                value: "",
-                validation: {
-                    required: true
+                Mailadresse: {
+                    elementType: "input",
+                    elementConfig: {
+                        type: "email",
+                        placeholder: "Mailadresse, eks mail@mail.com"
+                    },
+                    value: "",
+                    validation: {
+                        required: true,
+                        regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    },
+                    valid: false,
+                    touched: false
                 },
-                valid: false,
-                touched: false
-            },
-            Mailadresse: {
-                elementType: "input",
-                elementConfig: {
-                    type: "email",
-                    placeholder: "Mailadresse, eks mail@mail.com"
+                Spørsmål: {
+                    elementType: "textarea",
+                    elementConfig: {
+                        type: "text",
+                        placeholder: "Skriv ditt spørsmål her"
+                    },
+                    value: "",
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
-                value: "",
-                validation: {
-                    required: true,
-                    regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                },
-                valid: false,
-                touched: false
-            },
-            Spørsmål: {
-                elementType: "textarea",
-                elementConfig: {
-                    type: "text",
-                    placeholder: "Skriv ditt spørsmål her"
-                },
-                value: "",
-                validation: {
-                    required: true
-                },
-                valid: false,
-                touched: false
-            },
-        }
-    };
-    
-    clearForms = () => {
-        const updatedForm = {
-            ...this.state.questionForm
+            }
         };
         
-        for (let key in updatedForm) {
-            updatedForm[key].value = "";
-        }
-        
-        this.setState({questionForm: updatedForm, formIsValid: false});
-    };
+        this.state = this.initialstate;
+    }
     
+    clearForms = () => {
+        this.setState(this.initialstate);
+    };
+
     checkElementValidity(value, rules) {
         let isValid = true;
-        
+
         if (rules.required) {
             isValid = value.trim() !== "" && isValid;
         }
-        
+
         if (rules.regex) {
             isValid = rules.regex.test(value.trim())
         }
-        
+
         return isValid;
     }
-    
+
     checkFormValidity(updatedForm) {
         let formIsValid = true;
-        
+
         for (let inputIdentifier in updatedForm) {
             formIsValid = updatedForm[inputIdentifier].valid && formIsValid;
         }
-        
+
         return formIsValid;
     }
 
@@ -109,17 +110,19 @@ export class SendQuestion extends Component {
 
         this.setState({questionForm: updatedForm, formIsValid: this.checkFormValidity(updatedForm)});
     };
-    
+
     formSubmissionHandler = (event) => {
         event.preventDefault();
+        toast = null;
         this.setState({loading: true});
-        const formData = {};
         
+        const formData = {};
+
         for (let formElementIdentifier in this.state.questionForm) {
             formData[formElementIdentifier] = this.state.questionForm[formElementIdentifier].value;
         }
-        
-        const data = {Username: formData.Brukernavn, Email: formData.Mailadresse, Question: formData.Spørsmål, UpVotes: 0, DownVotes: 0};
+
+        const data = {Username: formData.Brukernavn, Email: formData.Mailadresse, Question: formData.Spørsmål};
 
         fetch("api/index/sendquestion/", {
             method: "Post",
@@ -128,16 +131,20 @@ export class SendQuestion extends Component {
                 "Content-Type": "application/json"
             }
         })
-            .then(response =>  {
+            .then(response => {
                 response.json();
                 this.setState({loading: false});
+                toast = <ToastDismissable
+                    toastTitle={"Spørsmål Sendt!"}
+                    toastMessage={"Spørsmålet ditt er blitt sendt. Du får en mail når det er behandlet"}/>;
+                this.clearForms();
             })
             .catch(error => this.setState({error: true}));
     };
-    
+
     createForm() {
         const formElements = [];
-        
+
         for (let key in this.state.questionForm) {
             formElements.push({
                 id: key,
@@ -145,41 +152,44 @@ export class SendQuestion extends Component {
             });
         }
         
-        //TODO REMEMBER TO SET AN ERROR MESSAGE!
-        let form;
-        this.state.loading ? form = <Spinner /> : form = (
-            <form onSubmit={this.formSubmissionHandler}>
-                {formElements.map(formElement => (
-                    <InputElement
-                        key={formElement.id}
-                        label={formElement.config.validation.required ? formElement.id + "*" : formElement.id}
-                        elementType={formElement.config.elementType}
-                        elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value}
-                        invalid={!formElement.config.valid}
-                        shouldValidate={formElement.config.validation}
-                        touched={formElement.config.touched}
-                        changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
-                ))}
-            </form>
-        );
-        
-        return form;
+        return this.state.error ?
+            <p className="ErrorMessage">Får ikke sendt spørsmål. Vennligst kontroller Internetttilgangen</p> :
+            this.state.loading ? <Spinner/> :
+                (<form onSubmit={this.formSubmissionHandler}>
+                        {formElements.map(formElement => (
+                            <InputElement
+                                key={formElement.id}
+                                label={formElement.config.validation.required ? formElement.id + "*" : formElement.id}
+                                elementType={formElement.config.elementType}
+                                elementConfig={formElement.config.elementConfig}
+                                value={formElement.config.value}
+                                invalid={!formElement.config.valid}
+                                shouldValidate={formElement.config.validation}
+                                touched={formElement.config.touched}
+                                changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
+                        ))}
+                    </form>
+                );
     }
-    
+
     render() {
         let form = this.createForm();
-        
-        return(
-            <div className="ContentArea">
-                <Header
-                infoText={SEND_INFO_TEXT}
-                >{TITLE}</Header>
-                {form}
-                <p className="Info">* Obligatorisk felt</p>
-                <button className="FormSubmitButton" onClick={this.formSubmissionHandler} disabled={!this.state.formIsValid}>Send Spørsmål</button>
-                <button className="ClearButton" onClick={this.clearForms}>Tøm Felter</button>
-            </div>
+
+        return (
+            <>
+                <div className="ContentArea">
+                    <Header
+                        infoText={SEND_INFO_TEXT}
+                    >{TITLE}</Header>
+                    {form}
+                    <p className="Info">* Obligatorisk felt</p>
+                    <button className="FormSubmitButton" onClick={this.formSubmissionHandler}
+                            disabled={!this.state.formIsValid}>Send spørsmål
+                    </button>
+                    <button className="ClearButton" onClick={this.clearForms}>Tøm Felter</button>
+                </div>
+                {toast}
+            </>
         );
     }
 }
