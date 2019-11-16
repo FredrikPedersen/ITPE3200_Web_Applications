d9676639df202430a84807dff51901d4f862c596
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-16 12:41:45 +0100

    Added container class for displaying UserQuestions

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/UserQuestions/UserQuestions.js
@@ -0,0 +1,12 @@
+﻿import React, {Component} from "react";
+
+export class UserQuestions extends Component {
+    
+    render() {
+        return(
+            <div>
+                Here the user questions will be shown!
+            </div>
+        )
+    }
+}
