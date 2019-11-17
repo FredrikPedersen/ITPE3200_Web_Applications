dcd33eda567fc47c5d65efa0ec2cb2997b182de6
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-17 14:11:28 +0100

    Added Category and Categories components

---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/Categories/Categories.js
@@ -0,0 +1,37 @@
+﻿import React from "react";
+import Category from "./Category/Category";
+
+const categories = (props) => {
+    return props.categories.map(category => {
+        let backgroundColor;
+        switch (category.id) {
+            case 1:
+                backgroundColor = "GreenBackground";
+                break;
+            case 2:
+                backgroundColor = "RedBackground";
+                break;
+            case 3:
+                backgroundColor = "YellowBackground";
+                break;
+            case 4:
+                backgroundColor = "BlueBackground";
+                break;
+            default:
+                backgroundColor = "GreenBackground";
+                break;
+        }
+        
+        return (
+            <Category
+                key={category.id}
+                title={category.categoryName}
+                backgroundColor={backgroundColor}
+                clickHandler={() =>props.clickHandler(category.categoryName)}
+            />
+        )
+    })
+};
+
+
+export default categories;
---
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/components/Categories/Category/Category.js
@@ -0,0 +1,14 @@
+﻿import React from "react"
+import "./Category.css";
+
+const category = (props) => {
+    const styleClasses = "CategoryBox " + props.backgroundColor;
+    
+    return (
+        <div className={styleClasses} onClick={props.clickHandler}>
+            <p>{props.title}</p>
+        </div>
+    );
+};
+
+export default category;
