7478b19c835e8d64ed9c33a516e3c5c49b0c695a
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-17 14:13:55 +0100

    Implemented categories. The user now has to choose a category before they see any questions and answers.

--- VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/FAQ/FAQ.js
+++ VyCustomerSupport/VyCustomerSupport/ClientApp/src/containers/FAQ/FAQ.js
@@ -4,6 +4,7 @@ import "../../Styles.css";
 import LoadingSpinner from "../../components/UI/LoadingSpinner/LoadingSpinner";
 import Header from "../../components/UI/Header/Header";
 import QuestionsAndAnswers from "../../components/QuestionsAndAnswers/QuestionsAndAnswers";
+import Categories from "../../components/Categories/Categories";
 
 const FAQ_INFO_TEXT = "Hva lurer du på? Her finner du svar på spørsmål som vi har besvart";
 const TITLE = "Spørsmål og Svar";
@@ -15,22 +16,45 @@ export class FAQ extends Component {
         super(props);
 
         this.state = {
+            categories: [],
             qas: [],
+            categorySelected: false,
             loading: true,
             error: false
         };
 
-        fetch("api/index/qas")
+        fetch("api/index/categories")
+            .then(response => response.json())
+            .then(categories => {
+                this.setState({categories: categories, loading: false});
+            })
+            .catch(error => {
+                this.setState({error: true});
+            });
+    }
+    
+    categoryClickedHandler = (categoryTitle) => {
+        this.setState({categorySelected: true});
+        
+        fetch("api/index/qaswithcategory?category=" + encodeURIComponent(categoryTitle))
             .then(response => response.json())
             .then(qas => {
-                this.setState({qas: qas, loading: false});
+                this.setState({qas: qas})
             })
             .catch(error => {
                 this.setState({error: true});
             })
+    };
+    
+    generateCategories() {
+        return (
+            <Categories
+                categories={this.state.categories}
+                clickHandler={this.categoryClickedHandler}/>
+        )
     }
 
-    qaContent() {
+    generateQandA() {
         return (
             <QuestionsAndAnswers
                 questionsAndAnswers={this.state.qas}/>
@@ -38,11 +62,18 @@ export class FAQ extends Component {
     }
 
     render() {
-        let qas =
-            this.state.error ?
-                <p className="ErrorMessage">Kan ikke laste inn spørsmål og svar. Vennligst kontroller Internetttilgangen </p> :
+        let categories =
+            this.state.error ? <p className="ErrorMessage">Kan ikke laste inn kategorier. Vennligst kontroller Internetttilgangen </p> :
                 this.state.loading ? <LoadingSpinner/> :
-                    this.qaContent();
+                    this.generateCategories();
+                
+        
+        let qas =
+            this.state.error ? <p className="ErrorMessage">Kan ikke laste inn spørsmål og svar. Vennligst kontroller Internetttilgangen </p> :
+                this.state.loading ? <LoadingSpinner/> :
+                    this.state.categorySelected ? this.generateQandA() :
+                        null;
+                        
 
 
         return (
@@ -50,6 +81,8 @@ export class FAQ extends Component {
                 <Header
                     infoText={FAQ_INFO_TEXT}
                 >{TITLE}</Header>
+                {categories}
+                <div style={{width: "100%", paddingTop: "3em"}}></div> {/*Empty div to create space */}
                 {qas}
             </div>
         );
